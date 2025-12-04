f0277bc8921f0256c642c1f6875f7a3d13bf5f14
Sandro <sandrolp41@hotmail.com>
2025-12-04 17:43:32 -0300

    feat/implementa classe tema feat/findall

---
+++ src/tema/services/tema.service.ts
@@ -0,0 +1,36 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Tema } from '../entities/tema.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class TemaService {
+  constructor(
+    @InjectRepository(Tema)
+    private temaRepository: Repository<Tema>,
+  ) {}
+
+  async findAll(): Promise<Tema[]> {
+    return await this.temaRepository.find({
+      relations: {
+        postagem: true,
+      },
+    });
+  }
+
+  async findById(id: number): Promise<Tema> {
+    const tema = await this.temaRepository.findOne({
+      where: {
+        id,
+      },
+      relations: {
+        postagem: true,
+      },
+    });
+
+    if (!tema) {
+      throw new HttpException('Tema não encontrado!', HttpStatus.NOT_FOUND);
+    }
+    return tema;
+  }
+}
