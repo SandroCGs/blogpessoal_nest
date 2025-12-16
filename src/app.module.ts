326152cbf46c4fad704783446dbcefa233a9f4e5
Sandro <sandrolp41@hotmail.com>
2025-12-16 20:12:23 -0300

    prepara o projeto para deploy com banco de dados diferente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Postagem } from './postagem/entities/postagem.entity';
-import { Tema } from './tema/entities/tema.entity';
-import { TemaModule } from './tema/tema.module';
-import { AuthModule } from './auth/auth.module';
-import { UsuarioModule } from './usuario/usuario.module';
-import { Usuario } from './usuario/entities/usuario.entity';
 import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
+import { ProdService } from './data/services/ProdService';
+import { PostagemModule } from './postagem/postagem.module';
+import { TemaModule } from './tema/tema.module';
 import { AppService } from './app.service';
+import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_blogpessoal',
-      entities: [Postagem, Tema, Usuario],
-      synchronize: true, //para ambiente de desenvolvimento
-    }), //inclusão do modulo
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+      imports: [ConfigModule],
+    }),
+    PostagemModule,
     TemaModule,
     AuthModule,
     UsuarioModule,
